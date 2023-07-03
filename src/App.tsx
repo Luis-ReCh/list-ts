142ffa64d930d1b9dee6d29090d5a0fa7bf1cfa5
luis <itsmluis@gmail.com>
2023-07-03 13:55:34 -0600

    Update the form

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,59 @@
+import { useState } from "react";
 import List from "./components/List";
 
-function App() {
-  return <List />;
-}
+export default function App() {
+  const [tasks, setTasks] = useState<string[]>([]);
+  const [text, setText] = useState(""); // el setText es el que ya trae la informacion cargada
 
-export default App;
+  function onAddItem() {
+    setTasks([text, ...tasks]);
+    setText(""); // vaciamos el setTexts para que el input no traiga nada
+  }
+
+  function onDelete(deleteIndex: number) {
+    tasks.splice(deleteIndex, 1);
+    setTasks([...tasks]);
+  }
+
+  function empty() {
+    setTasks([]);
+  }
+  return (
+    <div className="container pt-4">
+      <h1 className="text-center">Mis cosas por Hacer</h1>
+      <form action="" className="d-flex mb-3">
+        <input
+          className="form-control"
+          type="text"
+          id="text"
+          name="text"
+          onChange={(event) => setText(event.target.value)} //capturamos el valor el input
+          value={text}
+        />
+        <button
+          className="btn btn-outline-success"
+          type="button"
+          onClick={onAddItem}
+        >
+          +
+        </button>
+      </form>
+      <table className="table table-striped">
+        <tbody>
+          {tasks.map((task, index) => {
+            return (
+              <List
+                key={`koder-card ${index}`}
+                text={task}
+                onDelete={() => onDelete(index)}
+              />
+            );
+          })}
+        </tbody>
+      </table>
+      <button type="button" className="btn btn-success" onClick={empty}>
+        Borrar todo
+      </button>
+    </div>
+  );
+}
--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,89 +1,21 @@
-export default function List() {
+interface Props {
+  text: string;
+  onDelete: () => void;
+}
+
+export default function List(props: Props) {
   return (
-    <div className="container pt-4">
-      <h1 className="text-center">Mis cosas por Hacer</h1>
-      <form action="" className="d-flex">
-        <input className="form-control" type="search" />
-        <button className="btn btn-outline-success" type="submit">
-          +
+    <tr>
+      <td>{props.text}</td>
+      <td>
+        <button
+          type="button"
+          className="btn btn-danger"
+          onClick={props.onDelete}
+        >
+          Delete
         </button>
-      </form>
-      <table className="table table-striped">
-        {/* <thead>
-        <tr>
-          <th scope="col">First</th>
-          <th scope="col">Last</th>
-          <th scope="col">Handle</th>
-        </tr>
-      </thead> */}
-        <tbody>
-          <tr>
-            <td>comer</td>
-
-            <td>
-              <button type="button" className="btn btn-danger">
-                Delete
-              </button>
-            </td>
-          </tr>
-          <tr>
-            <td>correr</td>
-
-            <td>
-              <button type="button" className="btn btn-danger">
-                Delete
-              </button>
-            </td>
-          </tr>
-          <tr>
-            <td>llorar</td>
-
-            <td>
-              <button type="button" className="btn btn-danger">
-                Delete
-              </button>
-            </td>
-          </tr>
-          <tr>
-            <td>bailar</td>
-
-            <td>
-              <button type="button" className="btn btn-danger">
-                Delete
-              </button>
-            </td>
-          </tr>
-          <tr>
-            <td>ver la tv</td>
-
-            <td>
-              <button type="button" className="btn btn-danger">
-                Delete
-              </button>
-            </td>
-          </tr>
-          <tr>
-            <td>salir a pasear</td>
-
-            <td>
-              <button type="button" className="btn btn-danger">
-                Delete
-              </button>
-            </td>
-          </tr>
-          <tr>
-            <td>beber una serveza o 2</td>
-            <td>
-              <button type="button" className="btn btn-danger">
-                Delete
-              </button>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-      <button type="button" className="btn btn-success">
-        Borrar todo
-      </button>
-    </div>
+      </td>
+    </tr>
   );
 }
